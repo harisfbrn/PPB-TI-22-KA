e6c5f8496d2466e5f129369a927f3ffd468d925e
harisfbrn <113957916+harisfbrn@users.noreply.github.com>
2024-04-23 15:58:23 +0700

    Add files via upload

---
+++ Instagram/src/Constdata.js
@@ -0,0 +1,14 @@
+const UserObj = [
+    { id: 1, name: "Haris", username: "borisfebrian", img: require("../../Instagram/assets/images/poto2.jpg"), gender: "M", is_view: true, cap: "yaudah lah ya"},
+    { id: 2, name: "Angga", username: "oscar_oasis", img: require("../assets/images/poto3.jpg"), gender: "M", is_view: true, cap: "Hingga Engkau Bertahta" },
+    { id: 3, name: "BagasAji", username: "reallgood ", img: require("../assets/images/poto4.jpg"), gender: "M", is_view: true, cap: "Miror"},
+    { id: 4, name: "Subhan", username: "kevinn", img: require("../assets/images/poto5.jpg"), gender: "M", is_view: true, cap: "Mirir selfie" },
+    { id: 5, name: "Basor", username: "Bazzlightyear", img: require("../assets/images/poto6.jpg"), gender: "F", is_view: true, cap: "Senggol Dong" },
+    { id: 6, name: "Salma", username: "salma_amon", gender: "F", is_view: true },
+    { id: 7, name: "Teddy", username: "teddy", gender: "M", is_view: false },
+    { id: 8, name: "Daphne", username: "daphne_holy", gender: "F", is_view: false },
+    { id: 9, name: "Ferdy", username: "ferdyapriliyanto", gender: "M", is_view: false },
+    { id: 10, name: "Febri", username: "febrydfairuz", gender: "M", is_view: false }
+];
+
+export { UserObj };
